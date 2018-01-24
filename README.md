d80c2ffc01f8ec7d8538b3ae50826ece67fb1f83
Adrian Zucco <adrigabzu@gmail.com>
2018-01-24 14:44:25 +0100

    Improved README and AUC added

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Sholl analysis in R
 
-Sholl profile analysis based on mixed-effect models. Code adapted from SAS as described in:
+Initially described in the 50s [[1](#1)], Sholl analysis has been a standard method to quantitatively assess the morphological complexity of neurons. It is performed by counting dendrite intersections in concentric circles emanating from the soma of a cell at a fixed radius interval. Diverse programs can be used for processing the images [[2](#2)] but also different statistical approaches have been implemented to solve hypotheses based on these analyses.
+
+This [implementation in R](sholl_analysis_git.R) of a Sholl profile analysis based on mixed-effect models was adapted from a SAS as described in:
 
 > Wilson, M.D., Sethi, S., Lein, P.J., Keil, K.P., 2017. Valid statistical approaches for analyzing sholl data: Mixed effects versus simple linear models. Journal of Neuroscience Methods 279, 33–43. https://doi.org/10.1016/j.jneumeth.2017.01.003
 
+In this case, mixed-effect models allow accounting for the variability per mice and neurons as a random effect in order to evaluate differences between the conditions to test, described as fixed effects. For more information about linear mixed effects models and their implementation in R:
+
+* https://pagepiccinini.com/r-course/lesson-6-part-1-linear-mixed-effects-models/
+
+* http://rpsychologist.com/r-guide-longitudinal-lme-lmer
+
+### Features implemented:
+* Sholl profile plot with error bars (ggplot)
+* Overall difference between conditions considering autoregressive covariance
+* Statistical test of differences between conditions per radius
+* Area under the curve (AUC) calculation per neuron and statistical comparison based also on linear mixed-effect models.
+* Boxplots of AUCs per condition (ggplot2)
+
 Cite this code: [![DOI](https://zenodo.org/badge/118108207.svg)](https://zenodo.org/badge/latestdoi/118108207)
+____
+
+#1 Sholl, D.A., 1953. Dendritic organization in the neurons of the visual and motor cortices of the cat. J. Anat. 87, 387–406.
+
+#2 https://imagej.net/Sholl_Analysis
--- sholl_analysis_git.R
+++ sholl_analysis_git.R
@@ -1,27 +1,27 @@
 ##################################################################################
-#       Statistical analysis of Sholl profiles based on mixed-effect models     
-# 
+#       Statistical analysis of Sholl profiles based on mixed-effect models
+#
 # Adrian Gabriel Zucco
 # Center for Translational Neuromedicine
 # University of Copenhagen
 #
 # Adapted from:
-# Wilson, M. D., Sethi, S., Lein, P. J. & Keil, K. P.                            
-# Valid statistical approaches for analyzing sholl data: Mixed effects           
+# Wilson, M. D., Sethi, S., Lein, P. J. & Keil, K. P.
+# Valid statistical approaches for analyzing sholl data: Mixed effects
 # versus simple linear models. Journal of Neuroscience Methods 279, 33-43 (2017).
 #
 ##################################################################################
 
-#installing needed packages if missing
+############ Installing needed packages if missing ############################
 list.of.packages <-
-  c(
-    "data.table",
+  c("data.table",
     "lmerTest",
     "nlme",
     "reshape2",
     "ggplot2",
-    "plyr"
-  )
+    "plyr",
+    "dplyr",
+    "MESS")
 new.packages <-
   list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
 if (length(new.packages)) {
@@ -34,40 +34,48 @@ library(nlme)
 library(reshape2)
 library(ggplot2)
 library(plyr)
-
+library(dplyr)
+library(MESS)
 
 options(contrasts = c(factor = "contr.SAS", ordered = "contr.poly"))
 
-########### Function to generate the error bars ##############
-data_sd <- function(data, varname, groupnames) {
+########### Function to generate the error bars for the sholl profile ###########
+data_se <- function(data, varname, groupnames) {
   require(plyr)
   summary_func <- function(x, col) {
     c(mean = mean(x[[col]], na.rm = TRUE),
-      sd = sd(x[[col]], na.rm = TRUE))
+      se = sd(x[[col]], na.rm = TRUE) / sqrt(length(x[[col]])))
   }
   data_sum <- ddply(data, groupnames, .fun = summary_func,
                     varname)
-  data_sum <- rename(data_sum, c("mean" = varname))
+  data_sum <- plyr::rename(data_sum, c("mean" = varname))
   return(data_sum)
 }
 
-############ Data handling ################################
+############ Data parsing ################################
 
-# Open files
+# Open files, CHECK THE DATA FORMAT
 input_file = "example_data.tsv"
 df_toanalyze = read.csv(input_file, sep = "\t")
+
+# CHANGE THE VARIABLE NAMES ACCORDING TO THE COLUMN NAMES
+# or name them:
+# mouse_id. cell_id, radius, intersections, condition
 colnames(df_toanalyze)
 
+# Factorize categorical values
 df_toanalyze$radius <- as.factor(df_toanalyze$radius)
 df_toanalyze$mouse_id <- as.factor(df_toanalyze$mouse_id)
 df_toanalyze$cell_id <- as.factor(df_toanalyze$cell_id)
 
-# Open your tsv / csv file
-# Calculate SD for error bars
-sholl_plot = data_sd(df_toanalyze,
-                          varname = "intersections",
-                          groupnames = c("condition", "radius"))
 
+################# Sholl profile plot and analyisis ######################
+# Calculate SD for error bars
+sholl_plot = data_se(
+  data = df_toanalyze,
+  varname = "intersections",
+  groupnames = c("condition", "radius")
+)
 
 # Sholl profile plot with error bars, adapt variables to your column names
 ggplot(sholl_plot,
@@ -80,34 +88,74 @@ ggplot(sholl_plot,
   geom_line() +
   geom_point() +
   geom_errorbar(
-    aes(ymin = intersections - sd, ymax = intersections + sd),
+    aes(ymin = intersections - se, ymax = intersections + se),
     width = .2,
     position = position_dodge(0.05)
   ) +
-  scale_y_continuous(breaks = seq(0, 50, by = 5), limits = c(0, 50)) + # Change here the range of the y-axis
-  ylab("Intersections") + xlab("Radius") +
-  theme_classic() + scale_color_manual(values = c("red", "green")) # Change the colors of the lines here
+  # Change here the range of the y-axis
+  scale_y_continuous(breaks = seq(0, 50, by = 5), limits = c(0, 50)) +
+  # Change the labels if necessary
+  ylab("Dendritic Intersections") + xlab("Distance from the soma") +
+  # Change the colors of the lines here
+  theme_classic() + scale_color_manual(values = c("red", "green"))
 
 
 # Overall test for differences between the conditions using mixed effects model
-m <- nlme::lme(intersections ~  1 + condition,
-         data = df_toanalyze, random = ~ 1 | cell_id/radius , correlation=corAR1())
-anova(m)
+cond_test_corAR <- nlme::lme(
+  intersections ~  1 + condition,
+  data = df_toanalyze,
+  random = ~ 1 | cell_id / radius ,
+  correlation = corAR1()
+)
+summary(cond_test_corAR)
+anova(cond_test_corAR)
 
 # Sholl profile analysis with mixed effects models per radius
-m <-
+me_per_radius <-
   lmer(
-    intersections ~ 1 + radius  + condition + radius:condition +  (1 | mouse_id/cell_id),
+    intersections ~ 1 + radius  + condition + radius:condition +  (1 |
+                                                                     mouse_id / cell_id),
     df_toanalyze,
     REML = FALSE
   )
-print(summary(m, ddf = "Satterthwaite"), correlation = FALSE)
 
+test_summ = summary(me_per_radius, ddf = "Satterthwaite")
+print(test_summ, correlation = FALSE)
 
-############### p-values adjustement ###########################
-pvals = summary(m, ddf = "Satterthwaite")$coefficients[, 5]
-cond_pvals = pvals[grep("condition",names(pvals))]
-padj <- as.data.frame(p.adjust( cond_pvals , method = "BH"))
-padj$significance <- symnum(padj[,1], corr = FALSE, na = FALSE, cutpoints = c(0,0.001, 0.01, 0.05, 1), symbols = c("***", "**", "*", " "))
+################# p-values adjustement ###########################
+pvals = test_summ$coefficients[, 5]
+cond_pvals = pvals[grep("condition", names(pvals))]
+padj <- as.data.frame(p.adjust(cond_pvals , method = "BH"))
+padj$significance <-
+  symnum(
+    padj[, 1],
+    corr = FALSE,
+    na = FALSE,
+    cutpoints = c(0, 0.001, 0.01, 0.05, 1),
+    symbols = c("***", "**", "*", " ")
+  )
+
+print("Adjusted p-values: ")
 padj
 
+################# AUC of sholl profile analysis ##################
+df_auc <- df_toanalyze %>%
+  group_by(mouse_id, cell_id, condition) %>%
+  summarize(AUC = auc(intersections, radius, type = "spline"))
+
+# Boxplot of AUC
+ggplot(df_auc,
+       aes(y = AUC,
+           x = condition,
+           fill  = condition)) +
+  geom_boxplot() +
+  ylab("AUC of Sholl profile") +
+  # Change colors here
+  theme_classic() + scale_fill_manual(values = c("red", "green"))
+
+
+# Mixed-effect model of AUC
+m <- nlme::lme(AUC ~  1 + condition,
+               data = df_auc,
+               random = ~ 1 | mouse_id / cell_id)
+summary(m)
